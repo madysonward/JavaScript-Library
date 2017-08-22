8c9d6d5954e6866a170b94a2dd844074646c3c3e
Madyson Ward <MadysonWard94@gmail.com>
2017-08-22 08:40:27 -0400

    Added 7 js files on the very basics of JavaScript. Notes Guide, Data Types, Variables, Conditionals, Loops, Functions, Arrays, and Objects.

---
+++ 0.00_setup.js
@@ -0,0 +1,18 @@
+console.log("Welcome to JavaScript");
+
+//------------------------------------------------------------------------------------------------
+//Notes and Code: A Guide for All to Come
+//------------------------------------------------------------------------------------------------
+/* 
+Welcome to JavaScript! The lessons ahead will have both notes and code. Notes can be
+added like the above (lines 3, 4, and 5) for single-lined notes, with two forward slashes, or 
+like this current note (lines 6-) for multiple-lined notes, beginning with a forward 
+slash and an asterisk sign AND ending with an asterisk sign and forward slash. (Note the order!)
+
+As a guide, I will have different note styles for different situations:
+   1) Three single-lined notes in a row (two full lines of dashes & a topic title in the middle)
+   2) Multiple-lined notes (for the notes bulk!)
+   3) One single-lined note (full of dashes - for sectioning off notes/code)
+   3) Single-lined notes (for commenting out code that isn't meant to function with the other code
+ currently written on the page, or that is just an example)
+ */
