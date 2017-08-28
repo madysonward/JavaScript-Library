480bc47cf920108c87a419ecf85caaa70646db60
Madyson Ward <MadysonWard94@gmail.com>
2017-08-28 09:15:34 -0400

    Added content to .keys() file

---
+++ 0.07.2_objectsDotKeys.js
@@ -0,0 +1,15 @@
+//----------------------------------------------------------------------------------------
+//Object.keys()
+//----------------------------------------------------------------------------------------
+
+/* The Object.keys() method returns an array to represent all own enumerable properties of a given object. 
+This occurs in the same order that a for...in loops would give. It is coded like this: */
+
+Object.keys(obj);
+
+/* NOTE: obj acts as a parameter, and is defined as the object of which the enumerable own properties are 
+to be returned.
+
+Object.keys() returns an array of which the elements are strings corresponding the enumerable properties 
+found directly upon Object. */
+
